a9ebc62eb7506585b4122de59ee7ce89ed077002
Anton2101 <anton.kolbik@mail.ru>
2019-07-21 18:43:43 +0300

    'Update'

--- program/program.py
+++ program/program.py
@@ -1,7 +1,32 @@
-﻿import win32gui, win32api, time, codecs, asyncio, websockets, mysql.connector
+﻿import win32gui, win32api, time, codecs, asyncio, websockets, mysql.connector, bcrypt, sys
 from datetime import datetime, timedelta
-from keys import mas, first
+from keys import mas, first, letters, num, symb
 from multiprocessing import Process, Queue
+from win32api import GetSystemMetrics
+import kivy
+
+kivy.require('1.11.1')
+
+connect = mysql.connector.connect(host='localhost', port=3306, user='smallbro', password='smallbro', database='smallbro')
+cursor = connect.cursor()
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy import Config
+from kivy.core.window import Window
+from kivy.lang.builder import Builder
+
+Config.set('graphics', 'multisamples', '0')
+Config.set('graphics', 'resizable', '0')
+Config.write()
+Window.size = (600, 500)
+Window.clearcolor = (0.6, 0.85, 0.9176, 1)
+Window.left = (GetSystemMetrics(0) / 2) - (Window.size[0] / 2)
+Window.top = (GetSystemMetrics(1) / 2) - (Window.size[1] / 2)
+with open("file/my.kv", encoding='utf8') as f:
+    presentation = Builder.load_string(f.read())
+empty1 = 0
+empty2 = 0
 
 
 def new_time():
@@ -19,9 +44,7 @@ browsers = ['Google Chrome', 'Mozilla Firefox', 'Microsoft Edge', 'Internet Expl
 
 
 def sql_insert(window):
-    connect = mysql.connector.connect(host='localhost', port=3306, user='root', password='root',
-                                      database='small_bro')
-    cursor = connect.cursor()
+    global connect, cursor
     name = {'name': window}
     cursor.execute("""SELECT * FROM apps WHERE name = %(name)s""", name)
     app = cursor.fetchall()
@@ -34,7 +57,6 @@ def sql_insert(window):
         date = {'date': str(datetime.today().date()), 'app_id': int(app[0][0])}
     cursor.execute("""INSERT INTO sessions VALUES(null, null, %(date)s, %(app_id)s, null)""", date)
     connect.commit()
-    connect.close()
 
 
 def add_window(title, start_time, all_time, end_time):
@@ -61,9 +83,7 @@ def check_window(title):
 
 
 def sql_update(window, duration):
-    connect = mysql.connector.connect(host='localhost', port=3306, user='root', password='root',
-                                      database='small_bro')
-    cursor = connect.cursor()
+    global connect, cursor
     name = {'name': window}
     cursor.execute("""SELECT * FROM apps WHERE name = %(name)s""", name)
     sql_id = cursor.fetchall()[0][0]
@@ -77,7 +97,6 @@ def sql_update(window, duration):
     time_l = {'id': sql_id, 'time': duration}
     cursor.execute("""UPDATE sessions SET duration=%(time)s WHERE app_id = %(id)s ORDER BY id DESC LIMIT 1""", time_l)
     connect.commit()
-    connect.close()
 
 
 def idle(idle_s):
@@ -113,6 +132,7 @@ def server(q):
             url = str(url).split('/')
             q.put_nowait(url[2])
 
+    Window.hide()
     start_server = websockets.serve(get_url, '127.0.0.1', 8080)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
@@ -125,7 +145,8 @@ def update(f_window, get):
     sql_update(f_window, time_all)
 
 
-def main(q):
+def program(q):
+    Window.hide()
     first_window = window_search()
     window_start_time = str(datetime.now().time().strftime("%H:%M:%S"))
     add_window(first_window, window_start_time, '', '')
@@ -188,13 +209,151 @@ def main(q):
             time_to_idle_m = 0
 
 
-if __name__ == '__main__':
+process_1 = None
+process_2 = None
+
+
+def main():
     qe = Queue()
+    global process_1, process_2
     process_1 = Process(target=server, args=(qe,))
-    process_2 = Process(target=main, args=(qe,))
+    process_2 = Process(target=program, args=(qe,))
     process_1.start()
     process_2.start()
-    process_1.join()
-    process_2.join()
-    process_1.close()
-    process_2.close()
+
+
+def signin(email, password):
+    global connect, cursor
+    email = {'email': email}
+    cursor.execute("""SELECT * FROM users WHERE email = %(email)s""", email)
+    user = cursor.fetchall()
+    if len(user) == 0:
+        error = "Такой почты не существует"
+        return {'result': False, 'comment': error}
+    elif user[0][2] != password:
+        error = "Неправильный пароль"
+        return {'result': False, 'comment': error}
+    else:
+        return {'result': True}
+
+
+class LoginScreen(Screen):
+    input1 = ObjectProperty(None)
+    error1 = ObjectProperty(None)
+    input2 = ObjectProperty(None)
+    error2 = ObjectProperty(None)
+    pass
+
+    def check_email(self):
+        error_msg = 0
+        global empty1
+        if (len(self.input1.text) == 0) & (empty1 == 0):
+            empty1 = 1
+        elif (len(self.input1.text) == 0) & (empty1 == 1):
+            error_msg = 'Поле не может быть пустым'
+        elif self.input1.text.find('@') == -1:
+            error_msg = 'Введён неправильный или некорректный e-mail'
+        elif self.input1.text.split('@')[1].find('.') == -1:
+            error_msg = 'Введён неправильный или некорректный e-mail'
+        elif (self.input1.text.split('@')[0] == '') | (self.input1.text.split('@')[1] == ''):
+            error_msg = 'Введён неправильный или некорректный e-mail'
+        elif (len(self.input1.text.split('@')[1].split('.')[1]) < 2) | (
+                len(self.input1.text.split('@')[1].split('.')[1]) > 63):
+            error_msg = 'Введён неправильный или некорректный e-mail'
+        elif (self.input1.text.find(' ') != -1) | (len(self.input1.text) > 320):
+            error_msg = 'Введён неправильный или некорректный e-mail'
+        if isinstance(error_msg, str):
+            self.error1.text = error_msg
+            self.error1.size_hint = 1, None
+            self.error1.size = 0, 20
+        else:
+            if self.error1.text != "Такой почты не существует":
+                self.error1.text = ''
+                self.error1.size_hint = 1, 0
+                self.error1.size = 0, 0
+
+    def check_pass(self):
+        error_msg = 0
+        global empty2
+        if (len(self.input2.text) == 0) & (empty2 == 0):
+            empty2 = 1
+        elif (len(self.input2.text) == 0) & (empty2 == 1):
+            error_msg = 'Поле не может быть пустым'
+        elif (len(self.input2.text) < 6) & (empty2 == 1):
+            error_msg = 'Пароль должен состоять минимум из 6 символов'
+        else:
+            error_msg = self.symbol(letters)
+            if error_msg == 0:
+                error_msg = self.symbol(num)
+            if error_msg == 0:
+                error_msg = self.symbol(symb)
+        if isinstance(error_msg, str):
+            self.error2.text = error_msg
+            self.error2.size_hint = 1, None
+            self.error2.size = 0, 20
+        else:
+            if self.error2.text != "Неправильный пароль":
+                self.error2.text = ''
+                self.error2.size_hint = 1, 0
+                self.error2.size = 0, 0
+
+    def symbol(self, symb):
+        count = 0
+        for let in symb:
+            if (self.input2.text.lower().find(let) != -1) & (count <= 2):
+                count += 1
+                if self.input2.text.lower().find(let) != self.input2.text.lower().rfind(let):
+                    count += 1
+            if count == 2:
+                break
+        if count != 2:
+            return 'Пароль должен содержать минимум 2 буквы, цифры и спец. символа'
+        else:
+            return 0
+
+    def sign(self):
+        error = signin(self.input1.text, self.input2.text)
+        if not error['result']:
+            if error['comment'] == "Такой почты не существует":
+                self.error1.text = error['comment']
+                self.error1.size_hint = 1, None
+                self.error1.size = 0, 20
+            elif error['comment'] == "Неправильный пароль":
+                self.error2.text = error['comment']
+                self.error2.size_hint = 1, None
+                self.error2.size = 0, 20
+        elif error['result']:
+            self.error1.text = self.error2.text = ''
+            self.error1.size_hint = self.error2.size_hint = 1, 0
+            self.error1.size = self.error2.size = 0, 0
+            self.manager.current = 'program'
+
+
+class ProgramScreen(Screen):
+    pass
+
+    def start(self):
+        main()
+
+
+sm = ScreenManager()
+sm.add_widget(LoginScreen(name='login'))
+sm.add_widget(ProgramScreen(name='program'))
+
+
+class MyApp(App):
+
+    def build(self):
+        self.title = "Вход"
+        return sm
+
+    def on_stop(self):
+        global connect, process_2, process_1
+        process_1.kill()
+        process_2.kill()
+        connect.commit()
+        connect.close()
+
+
+if __name__ == '__main__':
+    MyApp().run()
